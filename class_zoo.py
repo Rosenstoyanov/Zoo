dd87e2f0435c7312777bf3d1177da901550d9219
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-04-01 13:45:34 +0300

    adding scr for database

---
+++ class_zoo.py
@@ -0,0 +1,7 @@
+class Zoo():
+    """docstring for Zoo"""
+    def __init__(self, animals, capacity, budget):
+        super(Zoo, self).__init__()
+        self.animals = animals
+        self.capacity = capacity
+        self.budget = budget
