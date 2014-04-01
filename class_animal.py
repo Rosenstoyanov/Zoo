2409985b6a8f8deb040ea22b4a91439f2f795415
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-04-01 13:29:31 +0300

    First commit

---
+++ class_animal.py
@@ -0,0 +1,10 @@
+class Animal():
+    """docstring for Animal"""
+    def __init__(self, species, age, name, gender, weight):
+        super(Animal, self).__init__()
+        self.species = species
+        self.age = age
+        self.name = name
+        self.gender = gender
+        self.weight = weight
+
