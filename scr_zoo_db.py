fca3adaa6cfc4b0de505683ba1c4f2c0b6f3f7a5
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-04-01 14:56:03 +0300

    script

--- scr_zoo_db.py
+++ scr_zoo_db.py
@@ -1,10 +1,10 @@
 import sqlite3
 
 def create_table_users(cursor):#add
-    cursor.execute('''CREATE TABLE users(id INTEGER PRIMARY KEY ASC, name Text, email Text)''')
+    cursor.execute('''CREATE TABLE users(id INTEGER PRIMARY KEY ASC, capacity int, budget real)''')
 
 
-conn = sqlite3.connect("dataBase.db")
+conn = sqlite3.connect("animals.db")
 
 cT1 = conn.cursor()
 create_table_users(cT1)
