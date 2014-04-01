1cd0e7b518a96af707cbd7ad99948cc6ccd0eae9
rosen66 <rosen.iv.stoyanov@gmail.com>
2014-04-01 15:19:58 +0300

    pushing interface

--- zoo_interface.py
+++ zoo_interface.py
@@ -7,21 +7,14 @@ class ZooInterface():
     def close_connection():
         self.conn.close()
 
-    def list_employees():#ne e taka
-        open_connection()
-        sql_query = "SELECT * FROM companies"
-        result = cursor.execute(sql_query)
+    def see_animals(self):
+        sql_query = "SELECT name, species, age, weight FROM animals"
+        result = self.cursor.execute(sql_query)
         for row in result:
             print(row)
         close_connection()
 
-    def see_animals():
-        sql_query = "SELECT name, species, age, weight FROM animals"
+    def accommodate (self, species, name, age, weight):
+        sql_query = "INSERT INTO animalsinZoo VALUES()"
 
-    def add(self, name, mail):
-        sql_query = "INSERT INTO users(name) VALUES(?,)"
-        sql_query2 = "INSERT INTO mail(mail) VALUES(?,)"
-        cursor.execute(sql_query, (name, ))
-        cursor.execute(sql_query2, (mail, ))
-        close_connection()
 
